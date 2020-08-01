118f34964d5531e3adc1010fd6d9c00801fc3efb
MARUF S <he.gamer454@gmail.com>
2020-08-02 04:58:46 +0700

    hello kotlin 1st

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "kotlin-strater"
+include("kotlin1")
---
+++ src/main/kotlin/app.kt
@@ -0,0 +1,19 @@
+/*
+Nama: Ma'ruf Salaffudin
+Kelas: XI RPL 2
+No: 13
+*/
+
+fun main(){
+    println("Hello Kotlin")
+}
+
+
+
+
+
+
+
+
+
+//help >:v
